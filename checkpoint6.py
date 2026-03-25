3426a2655c1188d922d3080353d1f20bebabf204
RaianSilex <chowd207@umn.edu>
2026-03-25 15:02:49 -0500

    updated checkpoint6

--- checkpoint6.py
+++ checkpoint6.py
@@ -41,6 +41,41 @@ def get_transform_cube(observation, camera_intrinsic, camera_pose):
     image, point_cloud = observation
 
     # TODO
+    if len(image.shape) > 2 and image.shape[2] == 4:
+        bgr = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+    else:
+        bgr = image
+
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, numpy.array([0, 60, 50]), numpy.array([180, 255, 255]))
+
+    pts = point_cloud[mask > 0, :3].astype(numpy.float64)
+    valid = numpy.isfinite(pts).all(axis=1)
+    pts = pts[valid]
+
+    if len(pts) < 10:
+        return None
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pts)
+    pcd, _ = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+
+    if len(pcd.points) < 10:
+        return None
+
+    obb = pcd.get_oriented_bounding_box()
+    center = numpy.array(obb.center)
+    R = numpy.array(obb.R)
+
+    t_cam_cube = numpy.eye(4)
+    t_cam_cube[:3, :3] = R
+    t_cam_cube[:3, 3] = center
+
+    t_robot_cube = numpy.linalg.inv(camera_pose) @ t_cam_cube
+
+    return t_robot_cube, t_cam_cube
+
+    
 
 def main():
 
@@ -65,6 +100,14 @@ def main():
 
         t_cam_cube = None
         # TODO
+        t_cam_robot = get_transform_camera_robot(cv_image, camera_intrinsic)
+        if t_cam_robot is None:
+            return
+        result = get_transform_cube([cv_image, point_cloud], camera_intrinsic, t_cam_robot)
+        if result is None:
+            print('Cube not detected.')
+            return
+        t_robot_cube, t_cam_cube = result
         
         # Visualization
         draw_pose_axes(cv_image, camera_intrinsic, t_cam_cube)
@@ -77,6 +120,8 @@ def main():
             cv2.destroyAllWindows()
 
             # TODO
+            grasp_cube(arm, t_robot_cube)
+            place_cube(arm, t_robot_cube)
     
     finally:
         # Close Lite6 Robot
